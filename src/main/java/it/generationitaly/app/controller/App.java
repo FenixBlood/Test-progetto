2f6611da760982c400900792bbb9786497807aff
crist <crist@Cristian.homenet.telecomitalia.it>
2024-09-27 15:42:45 +0200

    aggiunto syso"Hello World"

--- src/main/java/it/generationitaly/app/controller/App.java
+++ src/main/java/it/generationitaly/app/controller/App.java
@@ -3,8 +3,9 @@ package it.generationitaly.app.controller;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		System.out.println("Hello  World");
+		
 	}
 
 }
